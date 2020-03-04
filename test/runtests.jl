5b86633e7d9b85c4549ff01b749b35bd82d92c3f
Takuya Miyashita <miyashita@hydrocoast.jp>
2020-03-04 16:39:08 +0900

    add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,41 @@
 using VisClaw
 using Test
+using Printf
 
+# make a list
+exdir = joinpath(dirname(pathof(VisClaw)), "../example")
+filelist = readdir(exdir)
+filter!(x->occursin(".jl", x), filelist)
+map(s->filter!(x->!occursin(s, x), filelist), ["check", "fgmax", "run_examples"])
+map(s->filter!(x->!occursin(s, x), filelist), ["storm","seasurf","current"])
+
+# number
+nf = length(filelist)
+println(@sprintf("%d", nf)*" files are going to be tested...")
+
+## avoid warn
+using Plots
+plotlyjs()
+plt = plot()
+
+scratchdir = joinpath(CLAW, "geoclaw/scratch")
 @testset "VisClaw.jl" begin
-    # Write your own tests here.
+    # Write your own tests here. 
+    # for loop
+    for f in filelist
+        println(f)
+        try 
+            #include(joinpath(exdir,f))
+            @test_nowarn include(joinpath(exdir,f))
+        catch e
+            println("Failed "*f)
+
+            bt = backtrace()
+            msg = sprint(showerror, e, bt)
+            println(msg)
+
+            println()
+            continue
+        end
+    end
 end
