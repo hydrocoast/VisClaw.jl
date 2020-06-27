8224beb2289426335f87a2a205cc965b1c4ebbde
Takuya Miyashita <miyashita@hydrocoast.jp>
2020-06-27 10:08:36 +0900

    update loadfgmax: point_style

--- src/loadfgmax.jl
+++ src/loadfgmax.jl
@@ -14,24 +14,44 @@ function loadfgmax(loaddir::String, fg::VisClaw.FixedGrid; nval_save::Int64=fg.n
     dat = readdlm(joinpath(loaddir, filename), Float64)
     dat[dat.<-1e10] .= NaN
 
-    # assign
     ncol = 4 + 2(fg.nval) + 1
-    valall = permutedims(reshape(dat, (fg.nx, fg.ny, ncol)), [2 1 3])
-    topo = valall[:,:,4]
-    D = valall[:,:,5]
-    tD = valall[:,:,5+fg.nval]
-    tarrival = valall[:,:,end]
-    if fg.nval >= 2
-        v = valall[:,:,6]
-        tv = valall[:,:,6+fg.nval]
-    end
-    if fg.nval >= 5
-        M = valall[:,:,7]
-        tM = valall[:,:,7+fg.nval]
-        Mflux = valall[:,:,8]
-        tMflux = valall[:,:,8+fg.nval]
-        hmin = valall[:,:,9]
-        thmin = valall[:,:,9+fg.nval]
+    # assign
+    ##
+    if fg.style == 0 || fg.style == 1 || fg.style == 4
+        topo = dat[:,4]
+        D = dat[:,5]
+        tD = dat[:,5+fg.nval]
+        tarrival = dat[:,end]
+        if fg.nval >= 2
+            v = dat[:,6]
+            tv = dat[:,6+fg.nval]
+        end
+        if fg.nval >= 5
+            M = dat[:,7]
+            tM = dat[:,7+fg.nval]
+            Mflux = dat[:,8]
+            tMflux = dat[:,8+fg.nval]
+            hmin = dat[:,9]
+            thmin = dat[:,9+fg.nval]
+        end
+    elseif fg.style == 2 || fg.style == 3
+        valall = permutedims(reshape(dat, (fg.nx, fg.ny, ncol)), [2 1 3])
+        topo = valall[:,:,4]
+        D = valall[:,:,5]
+        tD = valall[:,:,5+fg.nval]
+        tarrival = valall[:,:,end]
+        if fg.nval >= 2
+            v = valall[:,:,6]
+            tv = valall[:,:,6+fg.nval]
+        end
+        if fg.nval >= 5
+            M = valall[:,:,7]
+            tM = valall[:,:,7+fg.nval]
+            Mflux = valall[:,:,8]
+            tMflux = valall[:,:,8+fg.nval]
+            hmin = valall[:,:,9]
+            thmin = valall[:,:,9+fg.nval]
+        end
     end
 
     if nval_save == 1;     fgmax = VisClaw.FGmax(topo,D,tD,tarrival)
