1bfd51b6e3fc789f6dff66806570798c6d56f3cb
Takuya Miyashita <miyashita@hydrocoast.jp>
2020-06-30 18:50:40 +0900

    update examples for CI

--- example/topo_GMT_ike.jl
+++ example/topo_GMT_ike.jl
@@ -5,6 +5,7 @@ using GMT: GMT
 simdir = joinpath(CLAW,"geoclaw/examples/storm-surge/ike/_output")
 topo = loadtopo(simdir)
 track = loadtrack(simdir)
+gauges = loadgauge(simdir)
 
 ## plot
 gmttopo(topo)
@@ -12,7 +13,6 @@ gmttoporange!(topo; lc=:red, lw=1.0)
 GMT.colorbar!(B="xa2000f1000 y+l\"(m)\"", D="jBR+w5.0/0.2+o-1.0/-0.2")
 gmttrack!(track; lc=:tomato, lw=1.0)
 GMT.coast!(D=:i, W=:thinnest, savefig="ike_topo.pdf")
-
 
 # projection and region GMT
 gmttrack(track; lc=:tomato, lw=1.0, J="M12/16", R=[-96 -84 20 36])
