a8080d1b01d4a49060f57f52ee4d87150e1c4ca6
Takuya Miyashita <miyashita@hydrocoast.jp>
2020-09-29 23:32:21 +0900

    add docstring

--- src/replaceunit.jl
+++ src/replaceunit.jl
@@ -1,5 +1,12 @@
-
 #################################
+"""
+    replaceunit!(fgmax::VisClaw.FGmax, unit::Symbol)
+    replaceunit!(gauge::VisClaw.Gauge, unit::Symbol)
+    replaceunit!(amrs::VisClaw.AMR, unit::Symbol)
+
+Time unit converter
+The second arg unit must be any one of :second, :minute, :hour, or :day.
+"""
 function replaceunit!(fgmax::VisClaw.FGmax, unit::Symbol)
 
     !haskey(timedict, unit) && error("Invalid specification of unit")
