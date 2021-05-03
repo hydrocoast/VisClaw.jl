4c96dff144b84dc47ab49fa4e6522f53189bc7ae
Takuya Miyashita <miyashita@hydrocoast.jp>
2021-05-03 18:51:12 +0900

    move examples to another repo

--- example/topo_ncfile.jl
+++
@@ -1,8 +0,0 @@
-using VisClaw
-
-simdir = joinpath(CLAW, "geoclaw/examples/tsunami/bowl-slosh-netcdf/_output")
-
-topo = loadtopo(simdir)
-
-gmttopo(topo; C=:relief)
-gmtcoastline!(topo; savefig="./bowl_netcdf.pdf")
